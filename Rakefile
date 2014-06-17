9bc55acac1a0924d2b934c2cb4e07078cc8c6210
Jonas Bygdén <github@bygden.nu>
2014-06-17 16:18:50 +0200

    Added zsh.after and files to my local branch

--- Rakefile
+++ Rakefile
@@ -268,6 +268,7 @@ def install_prezto
   run %{ mkdir -p $HOME/.zsh.before }
   run %{ mkdir -p $HOME/.zsh.after }
   run %{ mkdir -p $HOME/.zsh.prompts }
+  run %{ ln -nfs "$HOME/.yadr/zsh/zsh.after/*" "${ZDOTDIR:-$HOME}/.zsh.after/" }
 
   if ENV["SHELL"].include? 'zsh' then
     puts "Zsh is already configured as your shell of choice. Restart your session to load the new settings"
---
+++ zsh/zsh.after/jby.zsh
@@ -0,0 +1,35 @@
+PATH=/usr/local/bin:/usr/local/sbin:$HOME/bin:$HOME/.tmuxifier/bin:$PATH
+
+alias ls='/usr/local/bin/gls -F --color=auto'
+alias L='/usr/local/bin/gls -lLF --color=auto'
+alias l='/usr/local/bin/gls -lF --color=auto'
+alias la='/usr/local/bin/gls -laF --color=auto'
+alias emacs='emacs -nw'
+alias m='less -iM'
+alias more='m'
+alias mroe='m'
+alias pwd='echo $PWD'
+alias vu='vagrant up'
+alias vs='vagrant status'
+alias vssh='vagrant ssh'
+alias vd='vagrant destroy'
+alias vdf='vagrant destroy -f'
+alias vp='vagrant provision'
+
+function chpwd() {
+    emulate -L zsh
+    ls
+}
+
+function psg() {
+    emulate -L zsh
+    ps -ef | grep -v grep | egrep -i "$*"
+}
+
+set -o emacs
+
+eval $(gdircolors ~/.dir_colors/dircolors.256dark)
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+
+eval "$(tmuxifier init -)"
+unset -f getent
