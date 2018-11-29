6ed12a0d670f621c90cbbeb7be3c8ba5fa82a140
Jonas Bygdén <github@bygden.nu>
2018-11-29 16:17:15 +0100

    Changed path

--- zsh/zsh.after/jby.zsh
+++ zsh/zsh.after/jby.zsh
@@ -33,7 +33,7 @@ set -o emacs
 eval $(gdircolors ~/.dir_colors/dircolors.256dark)
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 # Only load Liquid Prompt in interactive shells, not from a script or from scp
-[[ $- = *i* ]] && source ~/src/div/liquidprompt/liquidprompt
+[[ $- = *i* ]] && source ~/src/github/liquidprompt/liquidprompt
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
 . ~/.iterm2_shell_integration.zsh
 /usr/local/bin/archey --offline
